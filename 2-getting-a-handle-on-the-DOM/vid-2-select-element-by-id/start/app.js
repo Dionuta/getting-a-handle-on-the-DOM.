ea357200123c59ec02203fc60669bfb4230487ec
Dionuta <diounta@gmail.com>
2018-12-30 18:42:12 -0800

    passed

--- 2-getting-a-handle-on-the-DOM/vid-2-select-element-by-id/start/app.js
+++ 2-getting-a-handle-on-the-DOM/vid-2-select-element-by-id/start/app.js
@@ -1,5 +1,8 @@
 const myHeading = document.getElementById('myHeading');
+const myButton = document.getElementById('myButton');
+const myTextInput = document.getElementById('myTextInput');
 
-myHeading.addEventListener('click', () => {
-  myHeading.style.color = 'red';
+//myButton changes myHeadding to whatever value typed into myTextInput with click event.
+myButton.addEventListener('click', () => {
+  myHeading.style.color = myTextInput.value;
 });
